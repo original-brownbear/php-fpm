3e9d1b116544c6bed15b120bb77b56989140dc6c
Armin Braun <armin.b@icanlocalize.com>
2016-02-01 00:44:30 +0100

    added php5.2

--- release.sh
+++ release.sh
@@ -2,5 +2,7 @@ cd 7.0.2
 docker build -t originalbrownbear/php-fpm:7.0.2 .
 cd ../5.6.17
 docker build -t originalbrownbear/php-fpm:5.6.17 .
+cd ../5.2.16
+docker build -t originalbrownbear/php-fpm:5.2.16 .
 cd ..
 docker push originalbrownbear/php-fpm
